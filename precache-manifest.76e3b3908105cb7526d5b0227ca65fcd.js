df44084f7ad805aac124ea31b62ba29b64f92d55
gropox <georg.the.moon@gmail.com>
2019-10-07 20:41:59 +0200

    Updates

---
+++ precache-manifest.76e3b3908105cb7526d5b0227ca65fcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09db62551e1b606015e0593b2d9b501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0226202d708ecb3b58ec",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "54a0e612ad3e2fb1c041",
+    "url": "/static/js/2.06732c49.chunk.js"
+  },
+  {
+    "revision": "0226202d708ecb3b58ec",
+    "url": "/static/js/main.7c74a1ca.chunk.js"
+  },
+  {
+    "revision": "1b81d733c6ff15a6c6b1",
+    "url": "/static/js/runtime-main.3945a188.js"
+  }
+]);
